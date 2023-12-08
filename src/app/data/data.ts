b3843e914ea57083b0472595e32cea2d26619907
Lucas Saladini <lucassaladini@hotmail.com>
2023-12-08 17:21:01 -0300

    Fetching data from mocked up data

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -1,14 +1,14 @@
 export const data = [
   {
-    "id": 1,
-    "photo": "aaa",
-    "title": "título",
-    "description": "descrição"
+    "id": "1",
+    "photo": "https://d585tldpucybw.cloudfront.net/sfimages/default-source/blogs/2023/2023-11/angular-logo-1200-628.png?sfvrsn=bf64b1ee_3",
+    "title": "Nova versão do Angular anunciada",
+    "description": "Angular anuncia que disponibilizará nova versão ainda este ano e a versão contará com diversas novas funcionalidades."
   },
   {
-    "id": 2,
-    "photo": "aaa",
-    "title": "título",
-    "description": "descrição"
+    "id": "2",
+    "photo": "https://cdn.vox-cdn.com/uploads/chorus_asset/file/23303819/FNgAy4JVkAs_Rbz.jpeg",
+    "title": "Novo filme anunciado",
+    "description": "Marvel anuncia novo filme do Cavaleiro da Lua"
   }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { data } from '../../data/data'
 
 @Component({
   selector: 'app-content',
@@ -7,9 +8,11 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './content.component.css'
 })
 export class ContentComponent implements OnInit {
-  photoCover: string = "https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2023/12/robert-downey-jr-homem-de-ferro.jpg?w=1024&h=576&crop=1"
-  contentTitle: string = "Minha Notícia"
-  contentDescription: string = "Olá, mundo"
+  photoCover: string = ""
+  contentTitle: string = ""
+  contentDescription: string = ""
+
+  private id: string | null = "0"
 
   constructor(
     private route: ActivatedRoute
@@ -17,7 +20,17 @@ export class ContentComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( value =>
-      console.log(value.get("id"))
+      this.id = value.get("id")
     )
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id: string | null) {
+    const result = data.filter(article => article.id == id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photo
   }
 }
